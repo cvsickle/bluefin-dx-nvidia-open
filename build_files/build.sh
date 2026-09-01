3cef49db73ab823833cf9bc7e873a7a1f8dd926e
cvsickle <vansicklecorey@gmail.com>
2026-09-01 21:22:09 +0000

    added signing verification

--- build_files/build.sh
+++ build_files/build.sh
@@ -51,3 +51,13 @@ dnf5 -y copr disable imput/helium
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+# Update policy.json to include signature verification for this image
+jq '.transports.docker["ghcr.io/cvsickle/bluefin-dx-nvidia-open"] = [
+  {
+    "type": "sigstoreSigned",
+    "keyPath": "/usr/share/pki/containers/cvsickle.pub",
+    "signedIdentity": {"type": "matchRepository"}
+  }
+]' /etc/containers/policy.json > /tmp/policy.json.tmp && \
+mv /tmp/policy.json.tmp /etc/containers/policy.json
