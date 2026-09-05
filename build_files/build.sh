0a91387dafa36ccc6814ec3e09e93a8ba7b79f44
cvsickle <vansicklecorey@gmail.com>
2026-09-05 00:26:02 +0000

    run module scripts with bash to prevent permission errors

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,20 +5,17 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-# Make module scripts executable
-chmod +x "$(dirname "$0")/modules"/*.sh
-
 # Run identity script
-"$(dirname "$0")/modules/identity.sh"
+bash "$(dirname "$0")/modules/identity.sh"
 
 # Run package installation script
-"$(dirname "$0")/modules/packages.sh"
+bash "$(dirname "$0")/modules/packages.sh"
 
 # Run font installation script
-"$(dirname "$0")/modules/fonts.sh"
+bash "$(dirname "$0")/modules/fonts.sh"
 
 # Run system configuration script
-"$(dirname "$0")/modules/system.sh"
+bash "$(dirname "$0")/modules/system.sh"
 
 # Run signature verification script
-"$(dirname "$0")/modules/signature.sh"
+bash "$(dirname "$0")/modules/signature.sh"
