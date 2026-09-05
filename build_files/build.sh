4bd9fae3d4eaebca8b78fa1305b593abb7736f29
cvsickle <vansicklecorey@gmail.com>
2026-09-05 00:19:20 +0000

    make module scripts executable

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,9 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
+# Make module scripts executable
+chmod +x "$(dirname "$0")/modules"/*.sh
+
 # Run identity script
 "$(dirname "$0")/modules/identity.sh"
 
